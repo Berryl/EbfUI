cc5fd51dcdb8b4f40434b491400a592a1c3a2100
EBF <fingerhut22@gmail.com>
2026-05-17 13:55:49 -0400

    Enhance: Introduce `CommandBinding` class with `is_enabled` property; add unit tests to verify behavior

--- tests/binding/command/test_command_binding.py
+++ tests/binding/command/test_command_binding.py
@@ -5,39 +5,55 @@ from ebf_ui.state.state_tracker import StateTracker
 
 
 class TestCommandBinding:
-    class TestWhenDisabled:
+    class TestIsEnabled:
+        class TestWhenDisabled:
 
-        def test_when_not_editing(self):
-            tracker = StateTracker(SimpleNamespace(name="original"))
-            validation = SimpleNamespace(is_valid=True)
+            def test_when_not_editing(self):
+                tracker = StateTracker(SimpleNamespace(name="original"))
+                validation = SimpleNamespace(is_valid=True)
 
-            sut = CommandBinding(tracker, validation)
-            assert tracker.is_editing == False
+                sut = CommandBinding(tracker, validation)
+                assert tracker.is_editing == False
 
-            assert not sut.is_enabled
+                assert not sut.is_enabled
 
-        def test_when_editing_but_not_dirty(self):
-            tracker = StateTracker(SimpleNamespace(name="original"))
-            validation = SimpleNamespace(is_valid=True)
-            sut = CommandBinding(tracker, validation)
+            def test_when_editing_but_not_dirty(self):
+                tracker = StateTracker(SimpleNamespace(name="original"))
+                validation = SimpleNamespace(is_valid=True)
+                sut = CommandBinding(tracker, validation)
 
-            tracker.begin_edit()
+                tracker.begin_edit()
 
-            assert tracker.is_editing
-            assert not tracker.is_dirty
+                assert tracker.is_editing
+                assert not tracker.is_dirty
 
-            assert not sut.is_enabled
+                assert not sut.is_enabled
 
-        def test_dirty_but_not_valid(self):
-            tracker = StateTracker(SimpleNamespace(name="original"))
-            validation = SimpleNamespace(is_valid=False)
-            sut = CommandBinding(tracker, validation)
+            def test_dirty_but_not_valid(self):
+                tracker = StateTracker(SimpleNamespace(name="original"))
+                validation = SimpleNamespace(is_valid=False)
+                sut = CommandBinding(tracker, validation)
 
-            tracker.begin_edit()
-            tracker.instance.name = "updated"
-            tracker.update_edit()
+                tracker.begin_edit()
+                tracker.instance.name = "updated"
+                tracker.update_edit()
 
-            assert tracker.is_dirty
-            assert not validation.is_valid
+                assert tracker.is_dirty
+                assert not validation.is_valid
 
-            assert not sut.is_enabled
+                assert not sut.is_enabled
+
+        class TestWhenEnabled:
+            def test_dirty_and_valid(self):
+                tracker = StateTracker(SimpleNamespace(name="original"))
+                validation = SimpleNamespace(is_valid=True)
+                sut = CommandBinding(tracker, validation)
+
+                tracker.begin_edit()
+                tracker.instance.name = "updated"
+                tracker.update_edit()
+
+                assert tracker.is_dirty
+                assert validation.is_valid
+
+                assert sut.is_enabled
